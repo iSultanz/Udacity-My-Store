aa3edc23885716abde1b5632b2c7fff4cba05fc3
Sultan Alsalmi <mrfalcooon@gmail.com>
2023-02-05 22:15:20 +0300

    last update

--- src/app/product/product-cart/product-cart.component.ts
+++ src/app/product/product-cart/product-cart.component.ts
@@ -7,16 +7,18 @@ import { ProductCartService } from '../services/productCart.service';
 @Component({
   selector: 'ums-product-cart',
   templateUrl: './product-cart.component.html',
-  styleUrls: ['./product-cart.component.css']
+  styleUrls: ['./product-cart.component.css'],
 })
-export class ProductCartComponent implements OnInit{
+export class ProductCartComponent implements OnInit {
   carts: Cart[] = [];
   total!: number;
   name: string = '';
   address: string = '';
   cardNumber: string = '';
-  constructor(private productCartService: ProductCartService,
-    private router: Router) { }
+  constructor(
+    private productCartService: ProductCartService,
+    private router: Router
+  ) {}
 
   ngOnInit() {
     this.carts = this.productCartService.getCart();
@@ -29,25 +31,21 @@ export class ProductCartComponent implements OnInit{
   }
   onUpdate() {
     this.total = this.productCartService.calculateTotal();
-}
-onCheckout() {
-  if (this.name === '' || this.address === '' || this.cardNumber === '') {
-    alert('Please fill all the fields');
-  } else
-  if(this.name.length < 3) {
-    alert('Please enter a valid name');
-  } else
-  if(this.address.length < 5) {
-    alert('Please enter a valid address');
-
-  }else
-  if(this.cardNumber.length < 16) {
-    alert('Please enter a valid card number');
-  } else {
-    this.productCartService.clearCart();
-    this.carts = [];
-    this.total = 0;
-    this.router.navigate(['/success']);
   }
-}
+  onCheckout() {
+    if (this.name === '' || this.address === '' || this.cardNumber === '') {
+      alert('Please fill all the fields');
+    } else if (this.name.length < 3) {
+      alert('Please enter a valid name');
+    } else if (this.address.length < 5) {
+      alert('Please enter a valid address');
+    } else if (this.cardNumber.length < 16) {
+      alert('Please enter a valid card number');
+    } else {
+      this.productCartService.clearCart();
+      this.carts = [];
+      this.total = 0;
+      this.router.navigate(['/success']);
+    }
+  }
 }
